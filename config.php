69dfa0d790d9d427b22a85143f63b0743c865db1
marvinvillanea <marvinvillanea1@gmail.com>
2022-11-17 23:21:21 +0800

    update

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
     define('DBUSER','root');
-    define('DBPWD','linkers1bs');
+    define('DBPWD','');
     define('DBHOST','localhost');
     define('DBNAME','spc_alumni');
-    define('HOST','http://localhost:7070');
+    define('HOST','http://localhost:80');
 ?>
